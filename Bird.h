99ca72299bafd8c374d59d7b349dcb86519e2834
omaraql2006-web <omaraql2006@gmail.com>
2025-11-12 15:15:07 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -1,14 +1,14 @@
 #ifndef BIRD_H
 #define BIRD_H
-
 #include "Animal.h"
+#include <string>
+using namespace std;
 
-//define class bird here 
-
-
-
-
-
-
-
+class Bird : public Animal {
+public:
+    double wingSpan;
+    Bird(string n, int a, double wing);
+    Bird();
+    ~Bird();
+};
 #endif
