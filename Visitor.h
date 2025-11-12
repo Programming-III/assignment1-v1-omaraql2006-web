69fcceeffc7e82be58e44104d199481a30623db7
omaraql2006-web <omaraql2006@gmail.com>
2025-11-12 15:17:30 +0200

    Define Visitor class with member variables and methods

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,15 @@
 #ifndef VISITOR_H
 #define VISITOR_H
-
 #include <string>
 using namespace std;
-
-//define visitor class here
-
-
-
-
-
-
-
+class Visitor {
+    private:
+        string visitorName;
+        int ticketsBought;
+        public:
+        Visitor(string name, int tickets);
+        Visitor();
+        ~Visitor();
+        void displayInfo();
+};
 #endif
